cc4206a06c532afaefff8cacb8053235dad9707b
schiehll <rn.schiehll@gmail.com>
2015-11-19 10:12:49 -0200

    add material-ui

--- app/states/Home.js
+++ app/states/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AlertContainer from 'react-alert';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Colors from 'material-ui/lib/styles/colors';
 
 class Home extends React.Component {
   constructor(props){
@@ -17,7 +19,12 @@ class Home extends React.Component {
     return(
       <div>
         <AlertContainer ref={(a) => global.message = a} {...this.alertOptions} />
-        <button onClick={this.addAlert}>Show Alert</button>
+        <RaisedButton 
+          label="SHOW ALERT"
+          backgroundColor={Colors.deepPurple800}
+          labelColor={Colors.grey50}
+          onClick={this.addAlert.bind(this)} 
+        />
       </div>
     );
   }
