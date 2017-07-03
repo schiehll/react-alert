3b6aa0a97b9b08d74022b78e19ce24d876b87a8e
Samuele Papa <samuele.papa@gmail.com>
2017-07-04 00:53:08 +0200

    Firefox icon compatibility (#31)
    * Firefox compatibility
    
    This fix makes the icon render in Firefox.
    
    * Update AlertMessage.spec.js.snap

--- src/alert/Close.js
+++ src/alert/Close.js
@@ -12,7 +12,7 @@ const Close = glamorous('div')({
     opacity: '0.5'
   }
 }, props => ({
-  background: `url('data:image/svg+xml;utf8,<svg fill="${props.glam.dark ? '#fff' : '#333'}" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 0 48 48" width="48"><path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>') no-repeat`,
+  background: `url('data:image/svg+xml;utf8,<svg fill="${props.glam.dark ? '%23fff' : '%23333'}" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 0 48 48" width="48"><path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>') no-repeat`,
   backgroundPosition: '50%',
   backgroundSize: '25px',
   backgroundColor: `${props.glam.dark ? '#444' : '#f3f3f3'} !important`
