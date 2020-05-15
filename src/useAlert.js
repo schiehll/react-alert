0ff8971ce6a1ddff5994f265711536d482f51cdc
Daniel Froz <48440185+danielfroz@users.noreply.github.com>
2020-05-15 19:29:52 -0300

    memoized useAlert()

--- src/useAlert.js
+++ src/useAlert.js
@@ -1,10 +1,12 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import DefaultContext from './Context'
 
 const useAlert = Context => {
   const alertContext = useContext(Context || DefaultContext)
-
-  return alertContext.current
+  const alert = useMemo(() => {
+    return alertContext.current
+  }, [alertContext])
+  return alert
 }
 
 export default useAlert
