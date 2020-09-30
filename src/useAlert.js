dc5fdce17a4311ea16e6a952cf0ee3d111ce1e54
sweeta123 <63840245+sweeta123@users.noreply.github.com>
2020-09-30 18:56:02 +0530

    Update useAlert.js

--- src/useAlert.js
+++ src/useAlert.js
@@ -1,9 +1,9 @@
-import { useContext, useMemo } from 'react'
+import { useContext, useMemo,useCallback } from 'react'
 import DefaultContext from './Context'
 
 const useAlert = Context => {
   const alertContext = useContext(Context || DefaultContext)
-  const alert = useMemo(() => {
+  const alert = useCallback(() => {
     return alertContext.current
   }, [alertContext])
   return alert
